73898882e5ef34b4df53346872abf7558a473126
easmith00 <easmith00@gmail.com>
2016-12-14 13:56:17 +1000

    Update deleter.py

--- deleter.py
+++ deleter.py
@@ -3,23 +3,22 @@ import sys
 import datetime
 
 def main():
-    save_out = sys.stdout
-    f = "deletion_log.log"
-    fsock = open(f, 'a')
+    save_out = sys.stdout                                                                                               # save the current stream (all print commands)                                                                                           # defines the log file
+    fsock = open("deletion_log.log", "a")                                                                               # open the log file for appending
     sys.stdout = fsock
-    for root, dirs, files in os.walk('D:', topdown=False):
-        for name in files:
-            duration = timeCalc(root, name)
-            if duration.days > 100:
+    for root, dirs, files in os.walk('D:', topdown=False):                                                              # generate tuple for directory and files
+        for name in files:                                                                                              # start loop to look for files that are too old
+            duration = timeCalc(root, name)                                                                             # call process to create a file age
+            if duration.days > 100:                                                                                     # check if file is aged out
                 try:
-                    print("deleted: ", os.path.join(root, name), datetime.datetime.today())
-                    #os.remove(os.path.join(root, name))
-                except OSError:
-                    print("failed to delete: ", os.path.join(root, name), datetime.datetime.today())
+                    print("deleted: ", os.path.join(root, name), datetime.datetime.today())                             # send deleted file details to log
+                    #os.remove(os.path.join(root, name))                                                                # delete file
+                except OSError:                                                                                         # just in case fails
+                    print("failed to delete: ", os.path.join(root, name), datetime.datetime.today())                    # send failure details to log
                     continue
             else:
                 continue
-        for name in dirs:
+        for name in dirs:                                                                                               # same as above for directories
             duration = timeCalc(root, name)
             if duration.days > 100:
                 try:
@@ -30,16 +29,16 @@ def main():
                     continue
             else:
                 continue
-    sys.stdout = save_out
-    fsock.close()
+    sys.stdout = save_out                                                                                               # stop logging
+    fsock.close()                                                                                                       # close file
 
 
 def timeCalc(root, att):
     try:
-        return datetime.datetime.today() - datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(root, att)))
+        return datetime.datetime.today() - datetime.datetime.fromtimestamp(os.path.getmtime(os.path.join(root, att)))   # work out age of file in days
     except PermissionError:
-        print("DENIED ACCESS: ", os.path.join(root, att), datetime.datetime.today())
-        return datetime.timedelta(days=0)
+        print("DENIED ACCESS: ", os.path.join(root, att), datetime.datetime.today())                                    # because something keeps failing on one file in test group
+        return datetime.timedelta(days=0)                                                                               # set to 0 to prevent attempted deletion of irritating file
 
 
 main()
